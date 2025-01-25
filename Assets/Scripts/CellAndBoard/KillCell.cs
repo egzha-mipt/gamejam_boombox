dd6fe6b6e09360771f3fea9d0b6ead34a10073d0
Kabanya <evonirazh@mail.ru>
2025-01-25 23:37:00 +0300

    не работает коллизция

--- Assets/Scripts/CellAndBoard/KillCell.cs
+++ Assets/Scripts/CellAndBoard/KillCell.cs
@@ -4,29 +4,27 @@ public class KillCell : MonoBehaviour
 {
     void Start()
     {
-        Debug.Log("kill cell is ready");
+        // Debug.Log("kill cell is ready");
     }
+    
+    // void KillPlayer(GameObject player)
+    // {
+    //     player.SetActive(false);
+    // }
 
+    // void Update() 
+    // {
+    // }
+    // void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     Debug.Log("KillCell works");
 
-
-    void KillPlayer(GameObject player)
-    {
-        player.SetActive(false);
-    }
-
-    void Update() 
-    {
-    }
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        Debug.Log("KillCell works");
-
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Father is dead!");
-            KillPlayer(other.gameObject);
-        } else{
-            Debug.Log("Father is alive!");
-        }
-    }
+    //     if (other.CompareTag("Player"))
+    //     {
+    //         Debug.Log("Father is dead!");
+    //         KillPlayer(other.gameObject);
+    //     } else{
+    //         Debug.Log("Father is alive!");
+    //     }
+    // }
 }
--- Assets/Scripts/Father.cs
+++ Assets/Scripts/Father.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class Father : MonoBehaviour
 {
     public float speed = 5f; 
-    public float verticalSpeed = 1f;
+    private float verticalSpeed = 0f;
+
+    // public float rollMult = -45;
+    // public float pitchMult = 30;
 
     void Update()
     {
@@ -15,26 +18,26 @@ public class Father : MonoBehaviour
         //grid limit for father
         float clampedX = Mathf.Clamp(transform.position.x, -0.5f, 8+2.2f);
         transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+
+        // transform.rotation = Quaternion.Euler(moveInput*pitchMult, -1*moveInput*rollMult,0);
+        // OnCollisionEnter2D();
     }
     
-    void OnTriggerEnter2D(Collider2D other)
+    public void Die()
     {
-        Debug.Log("KillCell works");
+        Debug.Log("Игрок погиб!");
+        // UnityEngine.SceneManagement.SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Father is dead!");
-            KillPlayer(other.gameObject);
-        } else{
-            Debug.Log("Father is alive (Why?");
+    public void OnCollisionEnter2D (BoxCollider2D collision)
+    {
+        GameObject collidedWith = collision.gameObject;
+        Debug.Log("OnCollisionEnter2D works");
+        Debug.Log(collidedWith.tag);
+        if (collidedWith.tag == "KillCell"){
+            Debug.Log("OnCollisionEnter2D works");
+            Destroy(collidedWith);
         }
     }
-
-    void KillPlayer(GameObject player)
-    {
-        player.SetActive(false);
-    }
-
-
 
 }
