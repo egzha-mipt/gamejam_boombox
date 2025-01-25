cb424f0d84a40a525a3aa1ed0304c35b7da3fa7a
Kabanya <evonirazh@mail.ru>
2025-01-25 13:18:18 +0300

    исправленная генерация тайлов

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,16 +4,16 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform father;
     public float smoothSpeed = 0.125f;
+    public float CamDeflectionY = 3f;
     private float fixedX;
-
     void Start()
     {
-        fixedX = transform.position.x;
+        fixedX = transform.position.x - 1; 
     }
 
     void LateUpdate()
     {
-        Vector3 targetPosition = new Vector3(fixedX, father.position.y, transform.position.z);
+        Vector3 targetPosition = new Vector3(fixedX, father.position.y + CamDeflectionY, transform.position.z);
 
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, targetPosition, smoothSpeed);
 
--- Assets/Scripts/CellAndBoard/CellGrid.cs
+++ Assets/Scripts/CellAndBoard/CellGrid.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 
-public class CellGrid : MonoBehaviour
+public class GridManager : MonoBehaviour
 {
-    public GameObject tileType1; 
+    public GameObject tileType1;
     public GameObject tileType2;
+    public GameObject tileType3;
+    public GameObject tileType4;
+
     public int rows = 20;
     public int columns = 5;
     public float tileSize = 1f;
@@ -19,12 +22,31 @@ public class CellGrid : MonoBehaviour
         {
             for (int y = 0; y < rows; y++)
             {
-                GameObject tilePrefab = Random.Range(0, 2) == 0 ? tileType1 : tileType2;
+                GameObject tilePrefab = GetRandomTile();
 
                 GameObject tile = Instantiate(tilePrefab, new Vector3(x * tileSize, y * tileSize, 0), Quaternion.identity);
 
                 tile.transform.parent = this.transform;
             }
+        }
+    }
+
+    GameObject GetRandomTile()
+    {
+        int randomIndex = Random.Range(0, 4);
+
+        switch (randomIndex)
+        {
+            case 0:
+                return tileType1;
+            case 1:
+                return tileType2;
+            case 2:
+                return tileType3;
+            case 3:
+                return tileType4;
+            default:
+                return tileType1;
         }
     }
 }
